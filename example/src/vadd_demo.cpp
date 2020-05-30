7c7c5dd73c6122727fa797e827ea6231174208b4
BenQuickDeNN <2413269761@qq.com>
2020-05-30 20:17:56 +0800

    add vadd demo

--- example/src/vadd_demo.cpp
+++ example/src/vadd_demo.cpp
@@ -1,6 +1,29 @@
 #include "intrin_helper.hpp"
 
+#include <cstdio>
+
+typedef float type;
+
+const unsigned long LEN = 16;
+
+type c[LEN], a[LEN], b[LEN];
+
 int main()
 {
+    using namespace std;
+
+    for (int i = 0; i < LEN; i++)
+    {
+        a[i] = 2.0;
+        b[i] = 3.0;
+    }
+
+    vadd<type>(c, a, b, LEN);
+
+    printf("vadd result is:\n");
+    for (const ele : c)
+        printf("%.1f\t");
+    printf("\n");
+
     return 0;
 }
