c0e128e2fd9b14cc2857a935da88711ee0df5fc8
BenQuickDeNN <2413269761@qq.com>
2020-05-30 16:41:39 +0800

    add instruction checker

--- kernel/configure.hpp
+++ kernel/configure.hpp
@@ -14,4 +14,26 @@
 /* Number of OpenMP Threads */
 // const int OMP_NUM_THREADs = 4;
 
+enum VEC_LENGTH
+{
+    VL64,
+    VL128,
+    VL256,
+    VL512
+};
+
+/**
+ * @brief check the max length of vector extension
+ */
+VEC_LENGTH checkVectorWidth();
+
+VEC_LENGTH checkVectorWidth()
+{
+    
+    /* 检测AVX512F */
+    /* 检测AVX */
+    /* 检测SSE */
+    /* 检测MMX */
+}
+
 #endif
